4a74d0692e8546a82c19a4ddd48eeafd5b4f0fa2
unknown <rbrtmt100@gmail.com>
2020-06-07 22:22:07 +0300

    add __manifest__.py file

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Task Schedular',
+    'author': 'Robert Mauti',
+    'depends': ['base'],
+    'version': '1.0',
+    'description': """
+	a task scheduling app
+""",
+    'auto_install': False,
+    'demo': [],
+    'data': [
+		
+	],
+    'installable': True,
+    'application': True,
+}
