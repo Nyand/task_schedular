500d6279ae1ef51da0d234d3c4ea67d010d0c378
unknown <rbrtmt100@gmail.com>
2020-06-14 16:59:54 +0300

    add constraints to fields field track on change

--- models/task_schedular.py
+++ models/task_schedular.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 from odoo import fields, models
 
 from odoo.exceptions import UserError
@@ -7,32 +8,35 @@ from odoo.exceptions import UserError
 class TaskSchedular(models.Model):
     _name='task.schedular'
     _description='Task Schedular'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _inherit = ['mail.thread','mail.activity.mixin']
+    _order = 'expiry_date ASC'
 	
-    name=fields.Char('Task name')
+    name=fields.Char('Task name', required=True)
     user_id = fields.Many2one(
         'res.users', string='Responsible', index=True, required=True,
          default=lambda self: self.env.user)
     description=fields.Text('Task description')
-    start_date = fields.Date('Start Date', index=True)
-    expiry_date = fields.Date('Expiry Date', index=True)
+    start_date = fields.Date('Start Date', index=True, required=True)
+    expiry_date = fields.Date('Expiry Date', index=True, required=True, track_visibility='onchange')
     state=fields.Selection([("new", "New"),
 	    ("in_progress", "In Progress"),
 	    ("completed", "Completed"),
 	    ("expired", "Expired")],
-	    default='new', index=True, group_expand="_expand_states")
+	    default='new', track_visibility='onchange', index=True, group_expand="_expand_states")
     last_modification = fields.Datetime(readonly=True)
 	
+	
+    _sql_constraints = [('project_date_greater', 'check(expiry_date >= start_date)', 'Error! project start-date must be lower than project expiry_date')]
+
 	
     def _expand_states(self, states, domain, order):
         return [key for key, val in type(self).state.selection]
 
-		
-		
+
     def write(self, values):
-	    values['last_modification']=fields.Datetime.now()
-	    return super(TaskSchedular, self).write(values)
-    
+        # help to "YYY-MM-DD"
+        values['last_modification']=fields.Datetime.now()
+        return super(TaskSchedular, self).write(values)		
 
     def unlink(self):
 	    for task in self:
